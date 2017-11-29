3d82eab6506943e2723e9a41f4f4bff41f094e6c
itsang06 <sang.sangphung@gmail.com>
2017-11-29 14:58:50 +0700

    Up_20171129

--- function.js
+++ function.js
@@ -5,5 +5,37 @@ module.exports = {
             marketcoincode = 'USDT-BTC';
         }
         return marketcoincode.toUpperCase();
+    },
+    getInfo: function (senderId, data_api){
+        var message = "✅ Hi " + data_api.Name + "\n";
+        message += "Số ID: " + senderId + "\n";
+        if (typeof data_api.Phone === 'undefined'){
+            message += "Phone: Updating \n";
+        }
+        else{   
+            message += "Phone: " + data_api.Phone + "\n";
+        }
+        if (typeof data_api.Email === 'undefined'){
+            message += "Email: Updating \n";
+        }
+        else{
+            message += "Email: " + data_api.Email + "\n";
+        }
+        var strApiKey = data_api.ApiKey;
+        message += "ApiKey: " + strApiKey.substr(0,4) + " ................ " + strApiKey.substr(-4) + "\n";
+        message += "Vốn đầu tư: " + data_api.Wallet + " USDT \n";
+        if (typeof data_api.Maxtrade === 'undefined'){
+            message += "Số crypto tối đa được giao dịch: Updating \n";
+        }
+        else{
+            message += "Số crypto tối đa được giao dịch: " + data_api.Maxtrade + "\n";
+        }
+        if (data_api.Active === true){
+            message += "Trạng thái: Hoạt động bình thường";
+        }
+        else{
+            message += "Trạng thái: Bị khóa" ;
+        }
+        return message;
     }
 };
